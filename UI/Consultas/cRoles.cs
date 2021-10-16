b0a52017e5d28007df522a2f73ebc355e6b0054a
Perla D. Canario Perez <perladenis163@gmail.com>
2021-10-16 07:47:33 -0400

    Agregar archivos de proyecto.

---
+++ UI/Consultas/cRoles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroADetalle.UI.Consultas
+{
+    public partial class cRoles : Form
+    {
+        public cRoles()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cRoles_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
